c1a84c0d373118aa26ab851f59f74d7a0a50a17d
shinjiber <shinjiber7@gmail.com>
2019-05-01 11:52:44 +0900

    create item model

---
+++ db/migrate/20190501024145_create_items.rb
@@ -0,0 +1,12 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.string :name, null: false
+      t.string :image, null: false
+      t.integer :price, null: false
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
