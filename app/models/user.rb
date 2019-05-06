57b9e3c0085e8090472b1d6d73d1809fa8d71b93
shinjiber <shinjiber7@gmail.com>
2019-05-06 15:19:19 +0900

    align the layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@
 
 class User < ApplicationRecord
   has_secure_password
+
+  validates :email, presence: true,
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: { case_sensitive: false }
+  validates :password_digest, presence: true, length: { minimum: 6 }
 end
